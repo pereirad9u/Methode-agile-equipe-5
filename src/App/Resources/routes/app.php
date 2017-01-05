af234661f4c9b8c9bd829c14933d65137f62a6de
Guillaume Launay <g.launay96@gmail.com>
2017-01-05 16:57:14 +0100

    add france connect

--- src/App/Resources/routes/app.php
+++ src/App/Resources/routes/app.php
@@ -1,3 +1,13 @@
 <?php
 
 $app->get('/', 'AppController:home')->setName('home');
+
+$app->get('/franceConnect',function (){
+    echo '<pre>';
+   var_dump($_GET);
+    echo '</pre>';
+     echo '<pre>';
+   var_dump($_SESSION);
+    echo '</pre>';
+
+});
